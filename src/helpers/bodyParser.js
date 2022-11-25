3aee3e8a4f79018b9be9f51cfef44f0f61d15d16
Michel John <michel.john@hotmail.com>
2022-11-25 14:43:03 -0300

    helper parse body

---
+++ src/helpers/bodyParser.js
@@ -0,0 +1,17 @@
+const bodyParser = (request, callback) => {
+  let body = '';
+
+  request.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  request.on('end', () => {
+    body = JSON.parse(body);
+
+    request.body = body;
+
+    callback();
+  });
+}
+
+module.exports = bodyParser;
